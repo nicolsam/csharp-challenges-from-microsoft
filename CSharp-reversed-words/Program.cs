1f1fdb24bed19a489b8219c24e0883672b12f48c
Nícolas Samuel <nicolas.lorrayne@gmail.com>
2024-06-21 17:02:59 -0300

    feat: Inverter as palavras mantendo a ordem da frase

---
+++ CSharp-reversed-words/Program.cs
@@ -0,0 +1,20 @@
+﻿string pangram = "The quick brown fox jumps over the lazy dog";
+string pangramReversedWords;
+
+string[] words = pangram.Split(" ");
+string[] reversedWords = new string[pangram.Length];
+
+Console.WriteLine(words);
+
+for (int i = 0; i < words.Length; i++)
+{
+    char[] letters = words[i].ToCharArray();
+    Array.Reverse(letters);
+
+    reversedWords[i] = String.Join("", letters);
+
+}
+
+pangramReversedWords = String.Join(" ", reversedWords);
+Console.WriteLine(pangramReversedWords);
+
